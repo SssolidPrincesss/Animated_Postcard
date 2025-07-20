de39ea9dc162fd54524285ffbcf08035641ab082
SolidPrincess <glden.chi@gmail.com>
2025-07-21 00:12:22 +0500

    Add Postcard

---
+++ src/Postcard.java
@@ -0,0 +1,24 @@
+package mkpostkardclout.autaticlyrepaint;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Postcard{
+	JFrame frame;
+	
+	public static void main(String[] args) {
+		Postcard card  = new Postcard();
+		card.go();
+	}
+
+	public void go() {
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		MyDrawPanel panel = new MyDrawPanel();
+		frame.getContentPane().add(BorderLayout.CENTER, panel);
+		frame.setSize(450, 700);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+}
+
